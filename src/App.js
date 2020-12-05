70a42e7e360ea06dfee375e36466a7b7aa324269
Rohit Agharkar <rohitsa40@gmail.com>
2020-12-06 02:07:56 +0530

    Added React router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import NavbarComponent from "./Components/Navbar/Navbar.component.jsx";
 import "./App.css";
-import SharkVRDisplayComponent from "./Pages/shark-vr-display/shark-vr-display.jsx";
+import SharkCardDisplay from "./Pages/shark-card-display/shark-card-display.jsx";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
-      <SharkVRDisplayComponent/>
+      <BrowserRouter>
+        <Route exact path="/" component={SharkCardDisplay} />
+      </BrowserRouter>
     </div>
   );
 }
