b6865cf92d237c88d38a0e98c791622dea35b841
Rohit Agharkar <rohitsa40@gmail.com>
2020-12-05 13:17:32 +0530

    working astronaut

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// import "@google/model-viewer"
+// import ModelViewer from 'react-model-viewer';
 
+// let h = "Horse.glb";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Astronaut</h1>
+
+    <div id="model" styles={{height:"500px",width:"500px"}}>
+        <model-viewer  src="assest/Astronaut.glb" alt="A 3D model of a robot" auto-rotate camera-controls background-color="#455A64"/>
+    </div>
     </div>
   );
 }
