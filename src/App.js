f42e73eb53bc4ec7ab4f506e341caa99881b6506
Rohit Agharkar <rohitsa40@gmail.com>
2020-12-05 15:47:14 +0530

    Added great hammerhead shark

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <h1>Astronaut</h1>
 
     <div id="model" styles={{height:"500px",width:"500px"}}>
-        <model-viewer  src="assest/Goblin_Shark_v1.glb" alt="A 3D model of a robot" auto-rotate camera-controls background-color="#455A64"/>
+        <model-viewer  src="assest/great_hammerhead_shark.glb" alt="A 3D model of a robot" auto-rotate camera-controls background-color="#455A64"/>
     </div>
     </div>
   );
