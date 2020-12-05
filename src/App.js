b3e301b7b8d7853633c32b6028bfc08e8fdd12e2
Rohit Agharkar <rohitsa40@gmail.com>
2020-12-05 14:14:02 +0530

    Added shark

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import "@google/model-viewer"
 // import ModelViewer from 'react-model-viewer';
-
+import  "./App.css"
 // let h = "Horse.glb";
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <h1>Astronaut</h1>
 
     <div id="model" styles={{height:"500px",width:"500px"}}>
-        <model-viewer  src="assest/Astronaut.glb" alt="A 3D model of a robot" auto-rotate camera-controls background-color="#455A64"/>
+        <model-viewer  src="assest/Goblin_Shark_v1.glb" alt="A 3D model of a robot" auto-rotate camera-controls background-color="#455A64"/>
     </div>
     </div>
   );
