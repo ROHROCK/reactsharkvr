cecd5d6301be2474672e3e2e3c3c336dc2ddbb49
Rohit Agharkar <rohitsa40@gmail.com>
2020-12-06 13:13:23 +0530

    Added Props

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import SharkCardDisplay from "./Pages/shark-card-display/shark-card-display.jsx"
 import SharkVRDisplayComponent from "./Pages/shark-vr-display/shark-vr-display.component.jsx";
 import {
   BrowserRouter,
-  Switch,
   Route,
-  Link
 } from "react-router-dom";
+import Home from "./Pages/Home/home.jsx";
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
       <BrowserRouter>
-        <Route exact path="/" component={SharkCardDisplay} />
-        <Route exact path="/shark/:sharkname" render={props=> <SharkVRDisplayComponent {...props} />} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/sharks" component={SharkCardDisplay} />
+        <Route exact path="/sharks/:s" render={props=> <SharkVRDisplayComponent {...props} />} />
         {/* <Route path="/shark/:sharkname" component={SharkVRDisplayComponent} /> */}
         {/* <Route exact path="/" component={SharkCardDisplay} /> */}
-        <Route exact path="/aboutus" component={AboutUs} />
+
       </BrowserRouter>
     </div>
   );
---
+++ src/Pages/Home/home.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+// import { aboutus } from "../About/aboutus.jsx";
+class Home extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Home;
