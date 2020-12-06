e6dc4b0b115385a6f871a8300435f9e4fc193990
kausarsayyed20 <kausarsayyed.20@gmail.com>
2020-12-06 12:37:23 +0530

    Added about us page and update Navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavbarComponent from "./Components/Navbar/Navbar.component.jsx";
 import "./App.css";
 import SharkCardDisplay from "./Pages/shark-card-display/shark-card-display.jsx";
+import AboutUs from "./Pages/AboutUs/Aboutus.jsx";
 import {
   BrowserRouter,
   Switch,
@@ -13,7 +14,8 @@ function App() {
     <div className="App">
       <NavbarComponent />
       <BrowserRouter>
-        <Route exact path="/" component={SharkCardDisplay} />
+        {/* <Route exact path="/" component={SharkCardDisplay} /> */}
+        <Route exact path="/" component={AboutUs} />
       </BrowserRouter>
     </div>
   );
---
+++ src/Pages/Technologies/Technologies.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Container ,Row ,Col } from "react-bootstrap";
+
+class TechnologiesUsed extends Component {
+    render() {
+        return (
+            <div>
+
+                <Container>
+               
+                </Container>
+
+            </div>
+        );
+    }
+}
+
+export default TechnologiesUsed;
