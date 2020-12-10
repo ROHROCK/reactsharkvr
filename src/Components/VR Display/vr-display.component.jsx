872ffbc36971c0ee9b924c87ef7d4c2d50673227
Rohit Agharkar <rohitsa40@gmail.com>
2020-12-10 21:17:43 +0530

    Added formatting and wiki links

--- src/Components/VR Display/vr-display.component.jsx
+++ src/Components/VR Display/vr-display.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./vr-display.component.css";
-import { Button, Card, CardDeck } from "react-bootstrap";
+import { Button, Card, CardDeck, Container } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
 import { withRouter } from "react-router-dom";
 
@@ -27,60 +27,64 @@ class VRDisplayComponent extends Component {
     // console.log(this.props.location.state.fetched.glbLink);
     return (
       <div className="container">
-        <model-viewer
-          src="/assest/Megalodon.glb"
-          alt="A 3D model of a robot"
-          auto-rotate
-          camera-controls
-          background-color="#455A64"
-          ar
-        />
-        <Card>
-          {/* <Card.Header>
+        <Container>
+          <model-viewer
+            src="/assest/Megalodon.glb"
+            alt="A 3D model of a robot"
+            auto-rotate
+            camera-controls
+            background-color="#455A64"
+            ar
+          />
+
+          <Card>
+            {/* <Card.Header>
             {this.props.location.state.fetched.sharkType}
           </Card.Header> */}
-          <Card.Body>
-            <Table responsive="lg"></Table>
-            <CardDeck>
-              {/*  */}
-              <Card
-                bg={"Success".toLowerCase()}
-                text={"Success".toLowerCase() === "light" ? "dark" : "white"}
-                style={{ width: "18rem" }}
-                className="mb-2"
-              >
-                <Card.Header>Shark Type</Card.Header>
-                <Card.Body>
-                  <Card.Title>
-                    {this.props.location.state.fetched.sharkType}
-                  </Card.Title>
-                </Card.Body>
-              </Card>
+            <Card.Body>
+              <Table responsive="lg"></Table>
+              <CardDeck>
+                {/*  */}
+                <Card
+                  bg={"Success".toLowerCase()}
+                  text={"Success".toLowerCase() === "light" ? "dark" : "white"}
+                  style={{ width: "18rem" }}
+                  className="mb-2"
+                >
+                  <Card.Header>Shark Type</Card.Header>
+                  <Card.Body>
+                    <Card.Title>
+                      {this.props.location.state.fetched.sharkType}
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
 
-              <Card
-                bg={"Primary".toLowerCase()}
-                text={"Primary".toLowerCase() === "light" ? "dark" : "white"}
-                style={{ width: "18rem" }}
-                className="mb-2"
-              >
-                <Card.Header>bodyLength</Card.Header>
-                <Card.Body>
-                  <Card.Title>
-                    {" "}
-                    {this.props.location.state.fetched.bodyLength}{" "}
-                  </Card.Title>
-                </Card.Body>
-              </Card>
-            </CardDeck>
+                <Card
+                  bg={"Primary".toLowerCase()}
+                  text={"Primary".toLowerCase() === "light" ? "dark" : "white"}
+                  style={{ width: "18rem" }}
+                  className="mb-2"
+                >
+                  <Card.Header>bodyLength</Card.Header>
+                  <Card.Body>
+                    <Card.Title>
+                      {" "}
+                      {this.props.location.state.fetched.bodyLength}{" "}
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+              </CardDeck>
 
-            <Button
-              variant="primary"
-              src={this.props.location.state.fetched.wikilink}
-            >
-              Click Here
-            </Button>
-          </Card.Body>
-        </Card>
+              <Button
+                variant="primary"
+                href={this.props.location.state.fetched.wikilink}
+                block
+              >
+                Click Here
+              </Button>
+            </Card.Body>
+          </Card>
+        </Container>
       </div>
     );
   }
